a7878c171b2946c230c5314c92d28d28d8e4dcdd
davidkapcsandi <david_kapcsandi@hotmail.com>
2026-04-09 14:55:00 +0100

    Fixed conflict

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+set -e
+
 cd /home/ec2-user/app
+
 npm install
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/app
+
+
 pkill node || true
+
 nohup npm start > app.log 2>&1 &
