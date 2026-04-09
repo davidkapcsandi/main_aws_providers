ed87e81616dc969b968ccf5fbef440a8c288ebce
davidkapcsandi <98022110+davidkapcsandi@users.noreply.github.com>
2026-04-09 14:11:25 +0100

    updated before_install.sh to sudo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-dnf install -y nodejs22 nodejs22-npm
+sudo dnf install -y nodejs22 nodejs22-npm
 
 mkdir -p /home/ec2-user/app
 chown -R ec2-user:ec2-user /home/ec2-user/app
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mkdir -p /home/ec2-user/app
-cd /home/ec2-user/
+cd /home/ec2-user/app
 npm install
